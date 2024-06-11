35b15ff89116d6c657c4950803f038bfce5f9939
Ahmed Hisham <77901768+devAhmeed@users.noreply.github.com>
2024-06-11 23:00:15 +0300

    add : verify-email blade template

---
+++ resources/views/Users/verify-email.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+    <x-card class="p-10 rounded max-w-lg mx-auto mt-24">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                Log In
+            </h2>
+            <p class="mb-4">Log in to create Your Notes</p>
+        </header>
+
+        Congratulations! Your account has been created successfully , Please verify your email address to continue.
+    </x-card>
+@endsection
