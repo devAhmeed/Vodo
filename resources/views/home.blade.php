0fa42048df9e19dfb81fbe63a69697ef31dda28d
Ahmed Hisham <77901768+devAhmeed@users.noreply.github.com>
2024-06-11 12:33:29 +0300

    add : new web views in blade template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    @include('partials._hero')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+    </div>
+    <div class="mt-6 p-4">
+    </div>
+@endsection
